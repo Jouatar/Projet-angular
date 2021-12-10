dd834bd61389c775645c541140d84665a506ead8
Kla <nathan35.minecraft@gmail.com>
2021-12-10 10:02:34 +0100

    Login presque OK, soucis avec les observables

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,6 +1,36 @@
 export class User {
+    private name: String;
+    private pkCoin: number;
+    private token : String = "";
+    private deck : Array<String> = [];
+
     constructor(
-        public username: string,
-        public pkCoin: number
-      ) {}
+      private usn: String,
+      private coin: number
+      ) {
+        this.name = usn;
+        this.pkCoin = coin;
+      }
+
+    getName(){
+      return this.name;
+    }
+
+    getCoin(){
+      return this.pkCoin;
+    }
+
+    getToken(){
+      return this.token;
+    }
+
+    getDeck(){
+      return this.deck;
+    }
+
+    setToken(str: String){
+      this.token = str;
+    }
+
+
 }
--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {User} from 'src/app/models/user.model';
+import { Observable } from 'rxjs';
+import 'rxjs/add/observable/of';
+
+export interface ApiLogin {
+  token:String
+}
+
+export interface UserLogin{
+  user: String,
+  coins: number,
+  deck: Array<String>
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserServiceService {
 
-  constructor() { }
+export class UserServiceService {
+  token: String = "";
+  constructor(private http: HttpClient) { }
+
+  login(usn : String): Observable<Boolean>{
+    let dataLogin: Boolean = false;
+    this.http.post<ApiLogin>('https://lostin70s.com/lpwebfront/api/poke-user/login',{name: usn}).subscribe((data: ApiLogin) => {
+      this.token = data.token;
+      
+      if(this.token){
+        dataLogin = true;
+      }
+      
+    });
+    return new Observable<Boolean>(dataLogin) ;
+  }
 }
+
