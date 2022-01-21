45567d62c50c935b9edea67e8a7b3c0c0aafb5a2
Kla <nathan35.minecraft@gmail.com>
2022-01-21 10:59:54 +0100

    fix sur l'API Login

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {User} from 'src/app/models/user.model';
-import { Observable } from 'rxjs';
+import { Observable,Subject } from 'rxjs';
 import 'rxjs/add/observable/of';
 
 export interface ApiLogin {
@@ -23,16 +23,20 @@ export class UserServiceService {
   constructor(private http: HttpClient) { }
 
   login(usn : String): Observable<Boolean>{
-    let dataLogin: Boolean = false;
+    var subject = new Subject<boolean>();
+    let dataLogin: boolean = false;
     this.http.post<ApiLogin>('https://lostin70s.com/lpwebfront/api/poke-user/login',{name: usn}).subscribe((data: ApiLogin) => {
+      console.log(data.token);
       this.token = data.token;
       
       if(this.token){
         dataLogin = true;
       }
+
+      subject.next(dataLogin);
       
     });
-    return new Observable<Boolean>(dataLogin) ;
+    return subject.asObservable() ;
   }
 }
 
