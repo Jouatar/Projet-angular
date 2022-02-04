8454471ef715272cfe66cd2076b122b726f24adb
Kla <nathan35.minecraft@gmail.com>
2022-02-04 11:09:51 +0100

    refresh navbar + refresh service

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { UserServiceService } from 'src/app/user-service.service';
+import { RefreshService } from 'src/app/refresh.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,7 +13,14 @@ export class NavBarComponent implements OnInit {
   imagePath = "../assets/logo_pokemon.png";
   user = "";
   usercoin = "";
-  constructor(private userService: UserServiceService) { }
+  private subscriptionName: Subscription;
+
+  constructor(private userService: UserServiceService, private refresh: RefreshService) { 
+    this.subscriptionName= this.refresh.getUpdate().subscribe
+             (message => { 
+              this.ngOnInit();
+    });
+  }
 
   ngOnInit(): void {
     this.userService.getData().subscribe((data:any) => {
