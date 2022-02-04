801958e77e0432e61dacdaf6444f1dedf862cd42
Ryan Guillerm <ryan.guillerm@gmaill.com>
2022-02-04 10:57:34 +0100

    Rechargement solde user OK

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -10,12 +10,26 @@ import { UserServiceService } from 'src/app/user-service.service';
 export class NavBarComponent implements OnInit {
   imagePath = "../assets/logo_pokemon.png";
   user = "";
+  usercoin = "";
   constructor(private userService: UserServiceService) { }
 
   ngOnInit(): void {
     this.userService.getData().subscribe((data:any) => {
       this.user = data.name;
-    });;
+    });
+    this.userService.getData().subscribe((data:any) => {
+      this.usercoin = data.coins;
+    });
   }
+
+  deleteUser() {
+    this.userService.delete();
+  }
+
+  // getSoldUser() {
+  //   this.userService.getData().subscribe((data:any) => {
+  //     this.user = data.pkCoin;
+  //   });
+  // }
 
 }
